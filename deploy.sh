02640546acdafbc165f1ef193db2a6a905ebf986
xuanmaihaier <1404111969@qq.com>
2022-01-04 14:51:23 +0800

    feat: 自动脚本修改

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
 set -e
 
-npm run build
-cd public
-git init 
-git add -A
-git commit -m "deploy"
+# 生成静态文件
+npm run docs:build
 
-git push -f git@{ip}:~/project.git master
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+echo 'blog.stride.fun' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
 cd -
