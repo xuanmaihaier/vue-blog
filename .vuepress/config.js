8f609961d3234b0607c79491bdc51d9ac7774966
崔耀 <cuiy@ipharmacare.net>
2022-03-18 15:23:21 +0800

    feat: 部分样式修改

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -146,7 +146,7 @@ module.exports = {
       'sakura',
       {
         num: 4, // 默认数量
-        show: true, //  是否显示
+        show: false, //  是否显示
         zIndex: 0, // 层级
         img: {
           replace: false, // false 默认图 true 换图 需要填写httpUrl地址
@@ -170,6 +170,38 @@ module.exports = {
           appId: 'wJBYwlHhL0WemskTB3g82D24-gzGzoHsz',
           appKey: '7M9gB0WRmYttxp82yI5UlJqD',
         },
+      },
+    ],
+    [
+      'ribbon-animation',
+      {
+        size: 90,   // 默认数据
+        opacity: 0.3,  //  透明度
+        zIndex: -1,   //  层级
+        opt: {
+          // 色带HSL饱和度
+          colorSaturation: "80%",
+          // 色带HSL亮度量
+          colorBrightness: "60%",
+          // 带状颜色不透明度
+          colorAlpha: 0.65,
+          // 在HSL颜色空间中循环显示颜色的速度有多快
+          colorCycleSpeed: 6,
+          // 从哪一侧开始Y轴 (top|min, middle|center, bottom|max, random)
+          verticalPosition: "center",
+          // 到达屏幕另一侧的速度有多快
+          horizontalSpeed: 200,
+          // 在任何给定时间，屏幕上会保留多少条带
+          ribbonCount: 2,
+          // 添加笔划以及色带填充颜色
+          strokeSize: 0,
+          // 通过页面滚动上的因子垂直移动色带
+          parallaxAmount: -0.5,
+          // 随着时间的推移，为每个功能区添加动画效果
+          animateSections: true
+        },
+        ribbonShow: false, //  点击彩带  true显示  false为不显示
+        ribbonAnimationShow: true  // 滑动彩带
       },
     ],
     [
