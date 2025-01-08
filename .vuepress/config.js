9a0c195ffb3b025372f85ba62e3d69577d548cbf
cui yao <cuiy@ipharmacare.net>
2025-01-08 17:40:42 +0800

    feat: 静态迁移

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   title: 'YoungDo',
-  base : './',
+  base: process.env.NODE_ENV === 'production' ? './' : '/',
   locales: {
     // 键名是该语言所属的子路径
     // 作为特例，默认语言可以使用 '/' 作为其路径。
